636edc9723b6a2f4417686c0e632432c3ad13c65
Ahmet Burak <ahmetburakbarakli@gmail.com>
2018-04-04 15:51:51 +0300

    pre-v0.0.3
    pre-v0.0.3

--- app/src/main/java/com/cclub/cclubbarcode/MainActivity.java
+++ app/src/main/java/com/cclub/cclubbarcode/MainActivity.java
@@ -1,7 +1,6 @@
 package com.cclub.cclubbarcode;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.design.widget.Snackbar;
